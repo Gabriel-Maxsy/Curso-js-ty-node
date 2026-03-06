2c85202e3b7b3dc883f4cd957cbab826d837b627
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2026-03-06 00:45:12 -0300

    feat: Adicionando aulas sobre middlewares.

--- node/aula13/src/controllers/homeController.js
+++ node/aula13/src/controllers/homeController.js
@@ -2,6 +2,6 @@ exports.paginaInicial = (req, res) => {
     res.render("index");
 };
 
-exports.trataPost = (req, res) => {
-    res.send(`teste de rota post: ${req.body.nome}`);
+exports.trataPost = (req, res, next) => {
+    res.send(`teste de rota post: ${req.body.cliente}`);
 };
