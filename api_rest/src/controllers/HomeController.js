c5fc7c604abdd779bdafb2048511bed76b897b2d
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2026-05-19 01:19:34 -0300

    feat: Adicionando e configurando sequelize, e criando models.

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from "../models/Aluno";
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      "tudoCerto": true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: "Yasmin",
+      sobrenome: "Pires",
+      email: "Yasmin@gmail.com",
+      idade: 21,
+      peso: 80,
+      altura: 1.80,
     });
+    res.json(novoAluno);
   }
 }
 
