b3b74a61cebde21ce4e3b9ed2f37f04f6c398b22
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2026-06-22 22:30:21 -0300

    feat: adicionando CRUD de usuários.

--- api_rest/src/controllers/UserController.js
+++ api_rest/src/controllers/UserController.js
@@ -2,19 +2,104 @@ import { password } from "../config/database";
 import User from "../models/User";
 
 class UserController {
+
+  // Create - Cria um novo usuário no banco:
   async store(req, res) {
     try {
 
       const novoUser = await User.create(req.body);
 
-      res.json(novoUser);
+      return res.json(novoUser);
 
     } catch(e) {
-      res.status(400).json({
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
 
+  }
+
+  // Index - retorna todos os usuários:
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch(e) {
+      return res.json(null);
+    }
+  }
+
+  // Show - Mostra um user específico:
+  async show(req, res) {
+    try {
+      const {id} = req.params;
+
+      const user = await User.findByPk(id);
+      return res.json(user);
+
+    } catch(e) {
+      return res.json(null);
+    }
+  }
+
+  // Update - Atualiza um user no banco:
+  async update(req, res) {
+    try {
+
+      if(!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não encontrado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if(!user) {
+        return res.status(400).json({
+          errors: ['User não encontrado'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+
+    } catch(e) {
+
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+
+    }
+  }
+
+  // Delete - Deleta um usuário do banco:
+    async delete(req, res) {
+    try {
+
+      if(!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não encontrado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if(!user) {
+        return res.status(400).json({
+          errors: ['User não encontrado'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+
+    } catch(e) {
+
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+
+    }
   }
 }
 
